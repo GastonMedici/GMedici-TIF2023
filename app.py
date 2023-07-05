f0ae92a2eec69456b6582843c68311924e475015
GastonMedici <gmedici@gmail.com>
2023-07-05 11:50:47 -0300

    Subiendo 2 app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ ma = Marshmallow(app): Se crea un objeto ma de la clase Marshmallow, que se util
 #app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/proyecto"
 
 #app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://GMedici:codoacodo@GMedici.mysql.pythonanywhere-services.com/GMedici$proyecto'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://GMedici:codoacodo@GMedici.mysql.pythonanywhere-services.com/GMedici$proyecto'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:root@GMedici.mysql.pythonanywhere-services.com/GMedici$proyecto'
 
 # Configura el seguimiento de modificaciones de SQLAlchemy a False para mejorar el rendimiento
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
